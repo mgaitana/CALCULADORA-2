deca3a48c4663b1ffc77e97655309b856367a388
mgaitana <mariogaitan15@gmail.com>
2022-11-09 21:37:36 -0500

    se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,16 @@
+function numeros(valor){
+    document.getElementById('resultado').value += valor;
+}
+
+function operacion(){
+    var opera = document.getElementById('resultado').value;
+    if(opera == 0){
+        document.getElementById('resultado').value = "sin operacion"
+    } else{
+        document.getElementById('resultado').value = eval(opera);
+    }
+}
+
+function resetear(){
+    document.getElementById('resultado').value = "";
+}
